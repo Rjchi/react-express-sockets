1366bf4dba1b0432ba109e7c4ddacb94c2130c8e
Rjchi <richardospina18@gmail.com>
2023-08-11 18:34:25 -0500

    configurar host

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import styles from "./App.module.css";
 
 // Le indicamos donde esta el servidor de webSockets esto nos devuelve un objeto
 // este objeto es el puente de conexion entre el back y el front
-const socket = io("http://localhost:5000");
+// const socket = io("http://localhost:5000");
+const socket = io();
 
 const App = () => {
   const [message, setMessage] = useState("");
