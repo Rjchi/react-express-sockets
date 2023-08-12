de6bba566e81d54b063e2fed7ef2c0d0951cf2c1
Rjchi <richardospina18@gmail.com>
2023-08-11 20:47:47 -0500

    despliegue

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import styles from "./App.module.css";
 // Le indicamos donde esta el servidor de webSockets esto nos devuelve un objeto
 // este objeto es el puente de conexion entre el back y el front
 // const socket = io("http://localhost:5000");
-const socket = io(process.env.REACT_APP_SOCKET_URL);
+const socket = io();
 
 const App = () => {
   const [message, setMessage] = useState("");
